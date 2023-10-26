dc7dd2f5256228bcfd4ce8754609c52412987322
Camille <camille.nibungco@gmail.com>
2023-10-26 16:31:31 -0700

    embed map

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
+$(document).ready(function(){
+    $('.tabs').tabs();
+  });
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+});
+
 $(document).ready(function() {
   $('.collapsible').collapsible();
 })
