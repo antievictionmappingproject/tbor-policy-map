f4a3cf07af2602cb7d4d110a1a46fd5db2526a0d
Camille <camille.nibungco@gmail.com>
2023-11-30 20:07:27 -0800

    fix vue

--- tbor/vue.config.js
+++ tbor/vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = {
   // Your other configuration options go here
+  publicPath: process.env.NODE_ENV === 'production' ? '/tbor-policy-map/' : '/'
 };
