ecf11a72ac757877ee7d9d2b591da46164959146
Camille Nibungco <camille.nibungco@gmail.com>
2025-09-22 13:39:58 -0700

    ci: deploy Vue CLI build to GitHub Pages

--- tbor/vue.config.js
+++ tbor/vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = {
   // Your other configuration options go here
-  publicPath: process.env.NODE_ENV === 'production' ? '/tbor-policy-map/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/tbor-policy-map/' : '/',
+  outputDir: 'dist',
 };
